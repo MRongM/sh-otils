d1e9e10165349b1caf3b3ffd9f5b6d924104a87f
MRongM <1399316847@qq.com>
2021-07-10 12:34:06 +0800

    auto update

--- tools.sh
+++ tools.sh
@@ -5,6 +5,13 @@ if [[ $1 == '-h' ]];then
   echo "  -h  help"
   echo "  -i  yum-init"
   echo "  -c  centos-cmd  exec:source tools.sh -c"
+  echo "  -w  crawl exec:bash tools.sh -w [name]"
+fi
+
+
+if [[ $1 == '-g' || $1 == 'g' ]];then
+  git commit -am "auto update"
+  git push origin master
 fi
 
 if [[ $1 == '-i' ]];then
@@ -14,4 +21,9 @@ fi
 if [[ $1 == '-c' ]];then
   source centos-cmd.sh
 fi
+
+if [[ $1 == '-w' ]];then
+  bash crawl.sh $2
+fi
+
 
